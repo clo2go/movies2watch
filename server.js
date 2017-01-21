9814c5d09b34df94a707162ac328f40c229a5765
clo2go <clo2go@gmail.com>
2017-01-21 07:15:09 -0600

    fixed server, added movie GET & POST, added handlebar inputs to view, not updating movie button

--- server.js
+++ server.js
@@ -3,28 +3,28 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 var mysql = require('mysql');
-
+//express server
 var app = express();
-// Serve static content in "public"
-app.use(express.static (__dirname + '/public'));
 // parse application
 app.use(bodyParser.urlencoded({
 	extended: false
 }))
 // override with POST having
-app.use(methodOverride('_method'))
-
+app.use(methodOverride('_method'));
 // exphbs
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
-}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// Serve static content in "public"
+app.use(express.static (__dirname + '/public'));
+
+
 
 // mysql connect
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: '',
+	password: 'utboot',
 	database: 'movie_planner_db'
 });
 
@@ -37,31 +37,32 @@ connection.connect(function(err) {
 	console.log('connected as id ' + connection.threadId);
 });
 
+//GET movies data
 app.get('/', function(req,res) {
 	connection.query('SELECT * FROM movies;', function(err, data) {
 		if (err) throw err;
-
 		res.render('index', {movies: data});
-	});
-});
-
+	})
+})
+//  POST new movie data added
 app.post('/create', function(req,res){
-	connection.query('INSERT INTO movies (movie) VALUES (?)', [res.body.plan], function(err,result) {
+	connection.query('INSERT INTO movies (movie) VALUES (?);', [req.body.movie], function(err,result) {
 		if (err) throw err;
 		res.redirect('/'); 
-	});
-});
+	})
+})
+//  PUT new movie into list
+app.put('/update', function(req, results) {
 
-app.put('/update', function(req, res) {
-
-	connection.query('UPDATE movies SET movie = ? WHERE id = ?', [req.body.movie, req.body.id], function(err,result){
+	connection.query('UPDATE movies SET movie = ? WHERE id = ?;', [req.body.movie, req.body.id], function(err,result){
 		if (err) throw err;
 		res.redirect('/');
-	});
-});
+	})
+})
 
 var port = 3000;
 app.listen(port);
+console.log('running through the 3000 like Drake');
 
 
 
